8746dd25dd084ac299e209dbd266e0592f2433ad
meral <meral@macbook-pro-von-musa.fritz.box>
2021-05-02 20:47:38 +0200

    day24
    arrays

--- java2021/src/day24_arrays/Array.java
+++ java2021/src/day24_arrays/Array.java
@@ -4,34 +4,43 @@ public class Array {
 
 	public static void main(String[] args) {
 		
-		int[] myList = {10,20,30,40};
+		int[] List = {10,20,30,40};
 		
 		String[] myArray = {"Apple","Orange","Watermelon"};
 		
-		System.out.println(myList[0]);
-		System.out.println(myList[1]);
-		System.out.println(myList[2]);
-		System.out.println(myList[3]);
+		System.out.println(List[0]);
+		System.out.println(List[1]);
+		System.out.println(List[2]);
+		System.out.println(List[3]);
 		
-		System.out.println(myList.length);
+		System.out.println(List.length);
 		System.out.println(myArray.length);
 		
-		for(int i=0;i<myList.length;i++) {
-			System.out.println(myList[i]);
+		for(int i=0;i<List.length;i++) {
+			System.out.println(List[i]);
 		}
 		
-		int[] scores = {80,90,100,35,60,72,64,30,10,5,3,99,89,80,90,100,35,60,72,64,30,10,5,3,99,89};
+		
+		
+		int[] score = {80,90,100,35,60,72,64,30,10,5,3,99,89,80,90,100,35,60,72,64,30,10,5,3,99,89};
 		
 		int sum = 0;
 		
-		for(int i=0;i<scores.length;i++) {
+		
+		
+		
+		for(int i=0;i<score.length;i++) {
 			
-			sum = sum + scores[i];
+			sum = sum + score[i];
 		}
 		
+		
+		
+		
 		System.out.println("Sum is:" + sum);
-		System.out.println("Total element in array is:" + scores.length);
-		System.out.println("Average is:" + sum/scores.length);
+		
+		System.out.println("Total element in array is:" + score.length);
+		System.out.println("Average is:" + sum/score.length);
 		
 		
 		
